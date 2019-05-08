262663414ad7d11c92b7d83527188837083d4550
anne <anne.thomas@mailbox.org>
2019-05-08 15:22:28 +0200

    Add more actions

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -3,17 +3,43 @@
 from hermes_python.hermes import Hermes
 
 INTENT_HOW_ARE_YOU = "a-thom:how_are_you"
+INTENT_GOOD = "a-thom:feeling_good"
+INTENT_BAD = "a-thom:feeling_bad"
+INTENT_OKAY = "a-thom:feeling_okay"
+
+INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_OKAY]
 
 
 def main():
     with Hermes("localhost:1883") as h:
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
+         .subscribe_intent(INTENT_GOOD, feeling_good_callback) \
+         .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
+         .subscribe_intent(INTENT_OKAY, feeling_okay_callback) \
          .start()
 
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "What do you think, bro. I'm excellent."
+    response = "I'm doing great. How about you?"
+    hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
+
+
+def feeling_good_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "That's awesome! I'm happy to hear that."
+    hermes.publish_end_session(session_id, response)
+
+
+def feeling_bad_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "Sorry to hear that. I hope you feel better soon."
+    hermes.publish_end_session(session_id, response)
+
+
+def feeling_okay_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "That's cool."
     hermes.publish_end_session(session_id, response)
 
 
