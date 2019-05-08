3146a6ecff96f429273d32ede1b855c32aa584c3
anne <anne.thomas@mailbox.org>
2019-05-08 16:26:07 +0200

    Weather

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 import ConfigParser
 from hermes_python.hermes import Hermes
 import pyowm
@@ -9,12 +9,9 @@ import io
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-INTENT_HOW_ARE_YOU = "a-thom:how_are_you"
-INTENT_GOOD = "a-thom:feeling_good"
-INTENT_BAD = "a-thom:feeling_bad"
-INTENT_OKAY = "a-thom:feeling_okay"
+INTENT_HOW_ARE_YOU = "a-thom:weather_at_place"
 
-INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_OKAY]
+INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_ALRIGHT]
 
 
 def main():
@@ -23,47 +20,26 @@ def main():
 
     with Hermes("localhost:1883") as h:
         h.owm = owm
-        h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
-         .subscribe_intent(INTENT_GOOD, feeling_good_callback) \
-         .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
-         .subscribe_intent(INTENT_OKAY, feeling_okay_callback) \
+        h.subscribe_intent(INTENT_HOW_ARE_YOU, weather_at_place_callback) \
          .start()
 
 
-def how_are_you_callback(hermes, intent_message):
+def weather_at_place_callback(hermes, intent_message):
     session_id = intent_message.session_id
 
     # set mood according to weather
     config = read_configuration_file(CONFIG_INI)
-    observation = hermes.owm.weather_at_place(config["secret"]["city"])
+    city = intent_message.slots.city.first().value
+
+    observation = hermes.owm.weather_at_place(city)
     w = observation.get_weather()
     temp = w.get_temperature('celsius')["temp"]
-    if temp >= float(config["secret"]["temperature_threshold"]):
-        response = "I'm feeling great! "
+    if temp >= float(15):
+        response = "It's freaking shining like a motherfucker outside"
     else:
-        response = "Not so good. "
-    response += "It's {} degrees in {}. How are you?".format(temp, config["secret"]["city"])
+        response = "It's fucking freezing outside."
 
     hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
-
-
-def feeling_good_callback(hermes, intent_message):
-    session_id = intent_message.session_id
-    response = "That's awesome! I'm happy to hear that."
-    hermes.publish_end_session(session_id, response)
-
-
-def feeling_bad_callback(hermes, intent_message):
-    session_id = intent_message.session_id
-    response = "Sorry to hear that. I hope you feel better soon."
-    hermes.publish_end_session(session_id, response)
-
-
-def feeling_okay_callback(hermes, intent_message):
-    session_id = intent_message.session_id
-    response = "That's cool."
-    hermes.publish_end_session(session_id, response)
-
 
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
     def to_dict(self):
@@ -80,5 +56,5 @@ def read_configuration_file(configuration_file):
         return dict()
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
